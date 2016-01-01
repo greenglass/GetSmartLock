0f5fd2e43e28a019632bca12f7ad8db4ecdbafd0
Derek Mardian <greenglasscode@gmail.com>
2016-01-01 12:01:36 -0800

    Takes input and reorganize data handling, added logic too. Extended test out but need to refactor to test suite so it cleans up test output of Gradle.

--- src/main/com/getsmart/lock/GetSmartLock.java
+++ src/main/com/getsmart/lock/GetSmartLock.java
@@ -1,87 +1,29 @@
 package main.com.getsmart.lock;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
+
+import main.com.getsmart.lock.blo.InformationBLO;
+import main.com.getsmart.lock.dto.InformationDTO;
 
 public class GetSmartLock {
   public static void main(String[] args) {
-    ArrayList<String> questions = new ArrayList<>();
-    ArrayList<String> answers = new ArrayList<>();
 
-    storeInformation("what is my name?", questions);
-    storeInformation("what date is it?", questions);
-    storeInformation("what grade did you learn cs?", questions);
+    InformationDTO appData = new InformationDTO();
+    InformationBLO appLogic = new InformationBLO();
 
-    storeInformation("derek", answers);
-    storeInformation("saturday", answers);
-    storeInformation("college", answers);
+    appData = appLogic.populateInformationDTO();
 
-    int count = questions.size();
-    System.out.println("Count: " + count);
+    System.out.println(appData.getQuestion());
 
-    for(int i=0; i < questions.size(); i++) {
-      String info = getInformation(i, questions);
-      int index = getIndexOfInformation(info, questions);
-      System.out.println(String.format("%d : %s", index, info));
-    }
+    Scanner userInputReader = new Scanner(System.in);
+    String userAnswer = userInputReader.next();
+    userInputReader.close();
 
-    for(int i=0; i < answers.size(); i++) {
-      String info = getInformation(i, answers);
-      int index = getIndexOfInformation(info, answers);
-      System.out.println(String.format("%d : %s", index, info));
-    }
+    appData.setUserAnswer(userAnswer);
+    appLogic.updateInformation(appData);
 
-    int index = getIndexOfInformation("what is my name?", questions);
-    String getQuestion = getInformation(index, questions);   
-    String getAnswer = getInformation(index, answers);
-    System.out.println(String.format("%s : %s", getQuestion, getAnswer));
-
-    try {
-      int randomInt = getRandomIndexList(questions); 
-      System.out.println("testing random: " + randomInt);
-    } catch (Exception e) {
-
-    }
-
+    boolean correctAnswer = appLogic.checkAnswer();
+    System.out.println("Was answer correct?: " + correctAnswer);
   }
 
-  /**
-   * Gets a random index from the provided list. 
-   *
-   * @param list The list to get a random index number from based on its size
-   * @throws Exception If the list size is < 1
-   */
-  public static int getRandomIndexList(ArrayList<String> list) throws Exception {
-    Random randomObj = new Random();
-    int randomIndex = 0;
-    int listSize = list.size();
-
-    if (listSize < 1) {
-      throw new Exception("List has size < 1 and can't get information");
-    }
-
-    randomIndex = randomObj.nextInt(listSize);
-    return randomIndex;
-  }
-
-  /**
-   * Stores the given information into the provided list.
-   */
-  public static void storeInformation(String info, ArrayList<String> storeToList) {
-    storeToList.add(info);  
-  }
-
-  /**
-   * Gets information from the provided list based on the index provided.
-   */
-  public static String getInformation(int index, ArrayList<String> getFromList) {
-    return getFromList.get(index);
-  }
-
-  /**
-   * Gets the index of the info provided. If no info is found a -1 is returned.
-   */
-  public static int getIndexOfInformation(String info, ArrayList<String> getFromList) {
-    return getFromList.indexOf(info);
-  }
 }
---
+++ src/main/com/getsmart/lock/blo/InformationBLO.java
@@ -0,0 +1,98 @@
+package main.com.getsmart.lock.blo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import main.com.getsmart.lock.dto.InformationDTO;
+
+/**
+ * The logic that will handle operations on the InformationDTO. Since the DTO is used for
+ * holding the data and retreiving the data this class handles manipulating the data.
+ */
+public class InformationBLO {
+
+  //Arrays are mimicing a database for the time being
+  private String[] questionsArray = {"what is my name?", "what date is it?", "what grade did you learn cs?"};
+  private String[] answersArray = {"derek", "saturday", "college"};
+  private ArrayList<String> questions = new ArrayList<>(Arrays.asList(questionsArray));
+  private ArrayList<String> answers = new ArrayList<>(Arrays.asList(answersArray));
+  private static InformationDTO appInfo;
+
+  /**
+   *  Public empty contructor.
+   */
+  public InformationBLO() {
+    appInfo = new InformationDTO();
+  }
+
+  /**
+   * Populates the InformationDTO with a random index, the corresponding answer and questions
+   * already defined in the databse.
+   */
+  public InformationDTO populateInformationDTO() {
+    int randomIndex = -1;
+    try {
+      randomIndex = getRandomIndexList(questions);
+    } catch (Exception e) {
+      //TODO : Exception handling
+    }
+    appInfo.setIndex(randomIndex);
+    appInfo.setQuestion(getInformation(randomIndex, questions));
+    appInfo.setAnswer(getInformation(randomIndex, answers));
+
+    return appInfo;
+  }
+
+  /**
+   * Gets a random index from the provided list. 
+   *
+   * @param list The list to get a random index number from based on its size
+   * @throws Exception If the list size is < 1
+   */
+  public int getRandomIndexList(ArrayList<String> list) throws Exception {
+    Random randomObj = new Random();
+    int randomIndex = 0;
+    int listSize = list.size();
+
+    if (listSize < 1) {
+      throw new Exception("List has size < 1 and can't get information");
+    }
+
+    randomIndex = randomObj.nextInt(listSize);
+    return randomIndex;
+  }
+
+  /**
+   * Update the InformationDTO with the provided DTO. Assuming this new DTO
+   * has some new or updated information.
+   */
+  public void updateInformation(InformationDTO updatedInfo) {
+    appInfo = updatedInfo;
+  }
+
+  /**
+   * Stores the given information into the provided list.
+   */
+  public void storeInformation(String info, ArrayList<String> storeToList) {
+    storeToList.add(info);  
+  }
+
+  /**
+   * Gets information from the provided list based on the index provided.
+   */
+  public String getInformation(int index, ArrayList<String> getFromList) {
+    return getFromList.get(index);
+  }
+
+  /**
+   * Check to see if the user answer and the stored answer match for a correct answer.
+   */
+  public boolean checkAnswer() {
+    boolean correctAnswer = false;
+
+    correctAnswer = appInfo.getAnswer().equals(appInfo.getUserAnswer());
+
+    return correctAnswer;
+  }
+}
